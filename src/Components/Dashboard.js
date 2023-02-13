e352804657958db9119f870ed9836064810bcdfd
ocmino <ocmino@gmail.com>
2023-02-13 09:07:10 +0100

    updates

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -26,7 +26,12 @@ export default function Dashboard() {
 return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100 sm:px-6 lg:px-8">
       <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-8xl font-bold">Welcome to your very own recipes application!</h1>
+        <h1 className="text-8xl font-bold">Welcome to this recipes application!</h1>
+        <br />
+        <p className="text-2xl">This application uses a Spring Boot backend, a React JS frontend and an MS SQL database deployed on Azure.</p>
+        <br />
+        <br />
+        <p className="text-2xl font-bold">Please select an option below:</p>
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
           <div className="w-full p-6 mt-6 text-left border rounded-xl sm:w-1/2">
             <button
@@ -37,8 +42,26 @@ return (
             </button>
             <br />
             <br />
+            <br />
+            <br />
             <button
-              className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+              className="w-full px-4 py-2 font-bold text-white bg-red-400 rounded hover:bg-red-700"
+              onClick={() => (window.location.href = "/updateuser")}
+            >
+              Update User
+            </button>
+            <br />
+            <br />
+            <button
+              className="w-full px-4 py-2 font-bold text-white bg-red-400 rounded hover:bg-red-700"
+              onClick={() => (window.location.href = "/deleteuser")}
+            >
+              Delete User
+            </button>
+            <br />
+            <br />
+            <button
+              className="w-full px-4 py-2 font-bold text-white bg-red-400 rounded hover:bg-red-700"
               onClick={logout}
             >
               Logout
